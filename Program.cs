9cd15b6934aac66dd03525f4a9e49a7ed782a00c
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-05-20 20:14:41 -0300

    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿double numero, contador, produto;
+
+   
+ Console.Write("Tabuada do numero: ");
+ numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0 || numero > 10)
+{
+    Console.WriteLine("digite numueros de 0 a 10");
+
+}
+else
+{
+    contador = 0;
+
+    while(contador <= 10)
+    {
+        produto = numero * contador;
+        Console.WriteLine($"{numero} x {contador++} = {produto}");
+    }
+}
+
+        
+
+    
+    
